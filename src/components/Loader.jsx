8c4f45a1aa84e035b33136e894b824045e679b92
sourav m s <115419964+appuzlotatheog@users.noreply.github.com>
2025-12-11 20:37:26 +0530

    Add files via upload

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,15 +4,28 @@ function Loader({ fullPage = false }) {
     return (
         <div className={`loader ${fullPage ? 'loader--full' : ''}`}>
             <div className="loader__container">
-                {/* Simple spinning ring loader */}
-                <div className="loader__ring">
-                    <div className="loader__ring-inner"></div>
+                {/* Premium animated logo loader */}
+                <div className="loader__logo">
+                    <span className="loader__letter" style={{ '--i': 0 }}>N</span>
+                    <span className="loader__letter" style={{ '--i': 1 }}>E</span>
+                    <span className="loader__letter" style={{ '--i': 2 }}>X</span>
+                    <span className="loader__letter" style={{ '--i': 3 }}>F</span>
+                    <span className="loader__letter" style={{ '--i': 4 }}>L</span>
+                    <span className="loader__letter" style={{ '--i': 5 }}>U</span>
+                    <span className="loader__letter" style={{ '--i': 6 }}>X</span>
                 </div>
 
-                {/* Text for full page */}
-                {fullPage && (
-                    <p className="loader__text">Loading</p>
-                )}
+                {/* Animated progress bar */}
+                <div className="loader__bar">
+                    <div className="loader__bar-fill"></div>
+                </div>
+
+                {/* Pulsing dots */}
+                <div className="loader__dots">
+                    <span className="loader__dot"></span>
+                    <span className="loader__dot"></span>
+                    <span className="loader__dot"></span>
+                </div>
             </div>
         </div>
     );
