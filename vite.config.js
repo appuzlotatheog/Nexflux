d247e6bb86a287852c59b5d105a8575cfd6dfcd0
sourav m s <115419964+appuzlotatheog@users.noreply.github.com>
2025-12-11 16:23:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig(({ mode }) => ({
     plugins: [
         react()
     ],
+    server: {
+        allowedHosts: ['nexflux-git-main-appuzlotatheogs-projects.vercel.app', 'nexflux-appuzlotatheogs-projects.vercel.app', 'nexflux-m49eip3h3-appuzlotatheogs-projects.vercel.app', 'nexflux.appuzlota.me', 'nexflux.hyprwings.tech', 'localhost']
+    },
     build: {
         // Disable source maps in production
         sourcemap: false,
